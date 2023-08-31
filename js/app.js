87c103a79d5cce7279b547aa8f47e41245e0ed9e
aya78 <ayak77431@gmail.com>
2023-08-31 17:26:26 +0300

    v-on

---
+++ js/app.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const app = createApp({
+    data() {
+        return {
+          count: 0
+        }
+      }
+})
+app.amount('#elzero-app')
